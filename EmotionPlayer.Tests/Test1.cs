a4c9c8a7297912692212e7174d270eade523836d
FaltoGH <sb051380us@gmail.com>
2025-01-26 02:41:58 +0900

    Upgrade

---
+++ EmotionPlayer.Tests/Test1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace EmotionPlayer.Tests
+{
+    [TestClass]
+    public class Test1
+    {
+        [STATestMethod]
+        public void RemoveSelectionTests()
+        {
+            var listBox = new ListBox();
+            listBox.Items.Add(null);
+            listBox.SelectedIndex = 0;
+            Assert.AreEqual(0, listBox.SelectedIndex);
+            listBox.RemoveSelection();
+            Assert.AreEqual(-1, listBox.SelectedIndex);
+        }
+    }
+}
